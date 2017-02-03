9a4d837a35e12415ceaa5e6d7efda3087b6dc966
NYurouski <nikolai.yurouski@gmail.com>
2017-02-03 10:23:15 +0300

    init

---
+++ oc/Source/ApplicationSettings/Impl/AppSettingsSingleton.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace oc.Source.ApplicationSettings.Impl
+{
+    public sealed class AppSettingsSingleton
+    {
+        private AppSettingsSingleton()
+        {
+        }
+
+        private static volatile AppSettingsSingleton _appSettingsSingletonInstance;
+
+        private static readonly object SyncRoot = new object();
+
+        public static AppSettingsSingleton GetInstance()
+        {
+            if (_appSettingsSingletonInstance == null)
+            {
+                lock (SyncRoot)
+                {
+                    if (_appSettingsSingletonInstance == null)
+                    {
+                        _appSettingsSingletonInstance = new AppSettingsSingleton();
+                    }
+                }
+            }
+            return _appSettingsSingletonInstance;
+        }
+
+        private ISettings AppSettings => CrossSettings.Current;
+
+        public string GetByKey(string key, string defaultValue = null)
+        {
+           var x = AppSettings.GetValueOrDefault(key, defaultValue);
+            return AppSettings.GetValueOrDefault(key, defaultValue);
+        }
+        public void Save(string key, string value)
+        {
+            AppSettings.AddOrUpdateValue(key, value);
+        }
+    }
+}
---
+++ oc/Source/ApplicationSettings/SettingsConstants/AppSettingsConstant.cs
@@ -0,0 +1,13 @@
+﻿namespace oc.Source.ApplicationSettings.SettingsConstants
+{
+    public static class AppSettingName
+    {
+        public const string Logged = "Logged";
+        public const string ProgramId = "ProgramId";
+        public const string ParticipantId = "ParticipantId";
+        public const string UserName = "UserName";
+        public const string Password = "Password";
+        public const string TimeSleep = "TimeSleep";
+
+    }
+}
