7b14f01d331a2066290b946a0e67fece348d3a61
NYurouski <nikolai.yurouski@gmail.com>
2017-02-03 10:27:54 +0300

    init

---
+++ oc/Source/Services/ApiService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Acr.UserDialogs;
+
+namespace oc
+{
+	public class ApiService
+	{
+		public class ServerApiException : Exception
+		{
+			public int ErrorCode { get; private set; }
+
+			public ServerApiException() { }
+			public ServerApiException(string message) : base(message) { }
+			public ServerApiException(string message, Exception inner) : base(message, inner) { }
+			public ServerApiException(string message, int errorCode) : base(message)
+			{
+				ErrorCode = errorCode;
+			}
+		}
+
+		private static ApiService instance;
+
+		public static ApiService Instance => instance ?? (instance = new ApiService());
+
+	    public ApiService() { }
+
+
+	    public async Task<TResponseModel> GetDataFromServerTask<TRequestModel, TResponseModel>(TRequestModel model, string url) where TRequestModel : class where TResponseModel : class, new()
+	    {
+             
+	        var response= await WebUtil.GetDataWithPostRequest(url, model);
+	        TResponseModel result = default(TResponseModel);
+	        if (!string.IsNullOrEmpty(response))
+	        {
+
+	            try
+	            {
+                    
+                    result= JsonConvert.DeserializeObject<TResponseModel>(response);
+                }
+	            catch (Exception ex)
+	            {
+
+                    System.Diagnostics.Debug.WriteLine(ex.ToString());
+                    
+                }
+	            
+	        }
+	        return result;
+	    }
+
+	}
+}
---
+++ oc/Source/Services/WebUtil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using ModernHttpClient;
+using Newtonsoft.Json;
+
+namespace oc
+{
+	public static class WebUtil
+	{
+
+		public static async Task<string> GetDataWithPostRequest<T>(string url, T requestModel)
+		{
+			string response = null;
+		    try
+			{
+				var httpClient = new HttpClient(new NativeMessageHandler());
+                httpClient.Timeout = TimeSpan.FromMinutes(2);
+                HttpResponseMessage data = await httpClient.PostAsync(url, new StringContent(JsonConvert.SerializeObject(requestModel, Formatting.Indented), Encoding.UTF8, "application/json"));
+                
+				if (data.StatusCode == HttpStatusCode.OK)
+				{
+					response = await data.Content.ReadAsStringAsync();
+				}
+
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine(ex.ToString());
+				return null;
+			}
+
+			return response;
+		}
+
+
+	}
+}
